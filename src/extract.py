10886645e5d72e07b9d57b9beccd0bc8b58c03f2
TheVelvetShadow <matt@thevelvetshadow.com>
2025-09-23 15:37:17 +0100

    Test Suite Built for testing Guardian API Class

---
+++ src/extract.py
@@ -0,0 +1,11 @@
+import requests
+import json
+from datetime import datetime
+
+def GuardianAPI():
+    pass
+
+
+
+URL = 'https://itunes.apple.com/search?term=beyonce&limit=5'
+p = pprint.PrettyPrinter(indent=4) # just to present the results nicely
---
+++ tests/test_extract.py
@@ -0,0 +1,49 @@
+import pytest
+import requests
+from datetime import datetime
+import json
+from src.extract import GuardianAPI
+from unittest.mock import patch
+
+
+
+# test_API_Key_Validation:
+def test_guardian_api_retrieves_api_key():
+    """Tests: creates instance, stores API key, rejects None/empty keys"""
+
+
+@pytest.mark.skip 
+def test_guardian_api_gets_article_data():
+    """Tests: that we are pulling Guardian's JSON """
+
+
+@pytest.mark.skip
+def test_search_articles_accepts_search_terms():
+    """Tests: search articles method - tests date & search terms"""
+
+
+# Processing of JSON Data
+
+@pytest.mark.skip
+@patch('src.extract.requests')
+def test_returns_required_fields(mock_requests):
+    """Tests: webPublicationDate, webTitle, webUrl all returned correctly"""
+
+
+@pytest.mark.skip
+def test_returns_publication_date_after_date_selected(mock_requests):
+    """Tests: publication date is after date_from value"""
+ 
+
+@pytest.mark.skip
+@patch('src.extract.requests')
+def test_returns_content_preview_max_1000_chars(mock_requests):
+    """Tests: content preview is limited to 1000 chars"""
+
+
+@pytest.mark.skip
+@patch('src.extract.requests')
+def test_returns_last_10_articles_max(mock_requests):
+    """Tests: the last 10 articles are provided & 10 articles are the limit """
+ 
+
